fe83d862a29b1abcc8efaa3811b5e00a1ceeb250
ooalur <ooalur@gmail.com>
2026-08-19 12:01:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Публичная конфигурация сайта.
+// Если вы ОБНОВЛЯЕТЕ существующее развертывание Apps Script, этот URL обычно остаётся прежним.
+// Если создадите НОВОЕ развертывание, вставьте сюда новый /exec URL.
+window.SOLFEGE_CONFIG = {
+  GOOGLE_SHEETS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbyPYoC34xNMNNPPpMXNJgQq69fy6mPZSOQfyph9wPGqV-Zc-XSsHuqEU1xPU19UeBSXog/exec',
+  SITE_VERSION: 'RC5'
+};
